a2ec02d7011e19f312a3e13e0a5e063e2949002e
Vigo Walker <vigopaul05@gmail.com>
2026-02-09 22:07:08 -0400

    Bump version to 1.0.6 in __init__.py and setup.py

--- axiomtradeapi/__init__.py
+++ axiomtradeapi/__init__.py
@@ -3,6 +3,6 @@ from axiomtradeapi.auth.login import AxiomAuth
 from axiomtradeapi.websocket._client import AxiomTradeWebSocketClient
 
 # Version
-__version__ = "1.0.3"
+__version__ = "1.0.6"
 
 __all__ = ['AxiomTradeClient', 'AxiomAuth', 'AxiomTradeWebSocketClient', '__version__', 'quick_login_and_get_trending', 'get_trending_with_token']
