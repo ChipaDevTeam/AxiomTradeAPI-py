b7828138a8c62ceb79afdaeae28ebc3a71d826c9
Vigo Walker <vigopaul05@gmail.com>
2026-04-18 12:52:29 +0200

    Add test_trending_tokens.py for client authentication and token tracking

---
+++ test_trending_tokens.py
@@ -0,0 +1,29 @@
+from axiomtradeapi import AxiomTradeClient
+
+EMAIL = "."
+PASSWORD = "." 
+
+client = AxiomTradeClient(
+    username=EMAIL,
+    password=PASSWORD
+)
+
+# Automatically logs in if no saved session exists
+# Will trigger an OTP flow if 2FA is required
+if not client.is_authenticated():
+    print("Please follow the login prompt...")
+    client.login() # Takes optional otp_callback
+
+# Simulate browser connection to initialize tracking sessions
+# This is required for some endpoints to register you as an "active user"
+# and to pass detailed bot or scraper protection checks.
+client.connect(
+    token_address="8P5kBTzvG7xyjTZRzi4ftzpy6mnL74AHLtHDqyDq44ST", # Optional: Simulate landing on a token page
+    sol_public_keys=["Address1...", "Address2..."], # Optional: Check balances during connect
+    evm_public_keys=["0xAddress1..."] 
+)
+
+print(f"Logged in as: {client.auth_manager.username}")
+
+print(client.auth.get_authenticated_headers())
+print(client.get_trending_tokens())
